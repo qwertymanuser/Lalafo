8d87a62c30bd968332e7b00dce2351feaa4b137e
qwertymanuser <marlenrai14@gmail.com>
2023-05-04 21:59:24 +0600

    второй урок

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -6,4 +6,5 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ('id', 'username', 'first_name', 'last_name',
+                  'email', 'date_joined', 'phone_number', 'profile_image')
