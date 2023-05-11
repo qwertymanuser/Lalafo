c2f4d28b14a489e7212bad32def9b9ea0262b5d7
qwertymanuser <marlenrai14@gmail.com>
2023-05-11 20:01:09 +0600

    hm1

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -8,31 +8,29 @@ class UserSerializer(serializers.ModelSerializer):
     user_posts = PostSerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name',
-                  'email', 'date_joined', 'phone_number', 'profile_image', 'user_posts')
-        
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_joined', 'phone_number', 'profile_image', 'user_posts')
+
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-        max_lenght=100, write_only=True
-    )
+        max_length=100, write_only=True)
     password2 = serializers.CharField(
-        max_lenght=100, write_only=True
-    )
+        max_length=100,write_only=True)
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'profile_image', 'password', 'password2' )
-
+        fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'profile_image', 'password', 'password2')
     
     def create(self, validated_data):
-        print(validated_data)
-        user = User.objects.create(
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            email=validated_data['email'],
-            phone_number=validated_data['phone_number'],
-            profile_image=validated_data['profile_image']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        if validated_data['password'] == validated_data['password2']:
+            user = User.objects.create(
+                username = validated_data['username'],
+                first_name = validated_data['first_name'],
+                last_name = validated_data['last_name'],
+                email = validated_data['email'],
+                phone_number = validated_data['phone_number'],
+                profile_image = validated_data['profile_image'],
+            )
+            user.set_password(validated_data['password2'])
+            user.save()
+            return user
+        else:
+            raise serializers.ValidationError("Пароли не совпадают!")
